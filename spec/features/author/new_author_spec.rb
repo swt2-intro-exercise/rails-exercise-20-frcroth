158ae7f0f431134e6617dce029112149d0a1146d
frcroth <frcroth@gmail.com>
2020-11-06 22:56:55 +0100

    Add test for #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe "New author page", type: :feature do
  
  it "should render withour error" do
-   visit new_author_path
+    visit new_author_path
+ end
+ it "should have input fields" do
+    visit '/authors/new'
+    expect(page).to have_field '#input'
  end
 end
