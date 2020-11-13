c66261822a5011fc0ac74098c97f95652f8c85c2
frcroth <frcroth@gmail.com>
2020-11-13 14:21:03 +0100

    Add test for author edit page in index

--- spec/features/author/author_overview_spec.rb
+++ spec/features/author/author_overview_spec.rb
@@ -3,12 +3,17 @@ require 'rails_helper'
 describe "Author overview page", type: :feature do
 
     it "should render" do
-        visit "/authors"
+        visit authors_path
     end
 
     it "should link to new author" do
-        visit "/authors"
+        visit authors_path
         expect(page).to have_link 'New', href: new_author_path
+    end
+
+    it "should link to edit" do
+        visit authors_path
+        expect(page).to have_link 'Edit', href: edit_author_path(1)
     end
 
     # it "should link to individual page" do
