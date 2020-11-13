85a6cafacb32252812a71790db55e623af05b4d1
frcroth <frcroth@gmail.com>
2020-11-13 14:13:52 +0100

    Add test for #11

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -4,9 +4,16 @@ describe "Show Author page", type: :feature do
 
     it "should have text" do
         @alan = FactoryBot.create :author
-
         visit author_path(@alan)
         expect(page).to have_text("Alan")
     end
+
+    it "should link to edit author page" do
+        @alan = FactoryBot.create :author
+        visit author_path(@alan)
+        expect(page).to have_link 'Edit', href: edit_author_path(@alan)
+    end
+
+
 
 end
