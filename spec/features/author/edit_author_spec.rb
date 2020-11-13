cc2c1cbda00843408db5914b9377000c8389526b
frcroth <frcroth@gmail.com>
2020-11-13 14:11:55 +0100

    Add harder test for edit

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -4,8 +4,18 @@ describe "Edit Author page", type: :feature do
 
     it "should render" do
         @alan = FactoryBot.create :author
-
         visit edit_author_path(@alan)
+    end
+
+    it "should update homepage" do
+        @alan = FactoryBot.create :author
+        visit edit_author_path(@alan)
+
+        page.fill_in 'author[homepage]', with: 'https://www.britannica.com/biography/Alan-Turing'
+        find('input[type="submit"]').click
+
+        visit authors_path
+        expect(page).to have_text("https://www.britannica.com/biography/Alan-Turing")
     end
 
 end
