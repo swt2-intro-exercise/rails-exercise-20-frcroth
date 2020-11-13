12c2b0ba0573674fb325bf7b8ab96d81f382c405
frcroth <frcroth@gmail.com>
2020-11-13 11:40:31 +0100

    Add test for #5

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,5 +13,13 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should be able to create new authors" do
+   visit new_author_path
+   page.fill_in 'author[first_name]', with: 'Edsger'
+   page.fill_in 'author[last_name]', with: 'Dijkstra'
+   page.fill_in 'author[homepage]', with: 'https://en.wikipedia.org/wiki/Edsger_W._Dijkstra'
+   find('input[type="submit"]').click
+  end
   
 end
