d0e5e82451f48a3f1ad832b27698800032bf9e7a
frcroth <frcroth@gmail.com>
2020-11-13 14:47:03 +0100

    Use factories more

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe Author, type: :model do
  
   it "should create author" do
-      author = Author.new({"first_name" => "Alan", "last_name" => "Turing", "homepage" => "http://wikipedia.org/Alan_Turing"})
+      author = FactoryBot.create :author
       expect(author.first_name).to eq("Alan")
   end
 
   it "should return full name" do
-    author = Author.new({"first_name" => "Alan", "last_name" => "Turing", "homepage" => "http://wikipedia.org/Alan_Turing"})
+    author = FactoryBot.create :author
     expect(author.name).to eq("Alan Turing")
   end
 
