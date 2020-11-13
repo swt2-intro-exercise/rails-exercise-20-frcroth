d32633701210e9dad3f66d6f85e88c700109eb57
frcroth <frcroth@gmail.com>
2020-11-13 14:19:27 +0100

    Add another test on author edit

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -18,4 +18,16 @@ describe "Edit Author page", type: :feature do
         expect(page).to have_text("https://www.britannica.com/biography/Alan-Turing")
     end
 
+    it "should not accept empty last_name as update" do
+        @alan = FactoryBot.create :author
+        visit edit_author_path(@alan)
+
+        page.fill_in 'author[last_name]', with: nil
+        find('input[type="submit"]').click
+
+        visit authors_path
+        expect(page).to have_text("Turing")
+        expect(@alan.errors.any?)
+    end
+
 end
