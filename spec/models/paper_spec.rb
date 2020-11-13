4ee5234707afabb94fc368004dd5953523f65273
frcroth <frcroth@gmail.com>
2020-11-13 14:49:47 +0100

    Add test for creating paper without title

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  
+  it "should not be valid without title" do
+    paper = Paper.new({"venue" => "Home", "year" => 1970})
+    expect(paper).to_not be_valid
+    expect(paper.errors.any?)
+    expect(paper.errors.full_messages.each).to include("Title can't be blank")
+  end
+
 end
