ad83c5839f608e0ae8abe67d72a7b94dd4737a95
frcroth <frcroth@gmail.com>
2020-11-13 11:25:20 +0100

    Add test for full name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -6,4 +6,9 @@ describe Author, type: :model do
     author = Author.new({"first_name" => "Alan", "last_name" => "Turing", "homepage" => "http://wikipedia.org/Alan_Turing"})
     expect(author.first_name).to eq("Alan")
  end
+
+ it "should return full name" do
+   author = Author.new({"first_name" => "Alan", "last_name" => "Turing", "homepage" => "http://wikipedia.org/Alan_Turing"})
+   expect(author.name).to eq("Alan Turing")
+ end
 end
